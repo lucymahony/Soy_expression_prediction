bb04106c8f7313fac0af9fa276b1c768f621598b
Lucy Mahony <lucy.mahony7@gmail.com>
2024-06-13 14:33:13 +0200

    script for running agro nt on soy

--- agro_nt_scripts/soy_agro_nt.sh
+++ agro_nt_scripts/soy_agro_nt.sh
@@ -1,32 +1,53 @@
 #!/bin/bash
-max_length=#
+
+# Parameters 
+max_length=4608 # 18432 *.25
 num_gpu=1
 per_device_batch_size=2
 gradient_acc_steps=16 # 16 * 2=global batch size of 32
 lr=3e-7
+train_epochs=6
+save_steps=500
+eval_steps=500 
+warmup_steps=50 
+logging_steps=500 
+
+# File paths
+training_script=/home/u10093927/workspace/dagw/DNABERT2/src/DNABERT_2/finetune/train.py
+model=/home/u10093927/workspace/dagw/DNABERT2/src/agro-nucleotide-transformer-1b
+data=/home/u10093927/workspace/dagw/Soybean/tmp/soy_1500up_0down_42
+outdir=/home/u10093927/workspace/dagw/Soybean/tmp/soy_1500up_0down_42
 
 echo "RUNNING SCRIPT"
-
-
-
-qig conda run -n transformers torchrun --nproc_per_node=${num_gpu} ../../DNABERT_2/finetune/train.py
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+qig conda run -n transformers torchrun --nproc_per_node=${num_gpu} ${training_script} \
+            --use_IA3 True \
+            --model_name_or_path ${model} \
+            --data_path ${data} \
+            --kmer -1 \
+            --run_name ia3 \
+            --model_max_length ${max_length}\
+            --per_device_train_batch_size $per_device_batch_size \
+            --per_device_eval_batch_size $per_device_batch_size \
+            --gradient_accumulation_steps $gradient_acc_steps \
+            --learning_rate $lr \
+            --num_train_epochs $train_epochs \
+            --fp16 \
+            --save_steps $save_steps \
+            --output_dir ${outdir} \
+            --evaluation_strategy steps \
+            --eval_steps $eval_steps \
+            --warmup_steps $warmup_steps \
+            --logging_steps $logging_steps \
+            --overwrite_output_dir True \
+            --log_level info \
+            --find_unused_parameters False \
+            -zslot 1 \
+            -zmem 64G \
+            -zgpu \
+            -zgpuslots 1 \
+            -zo ia3.log -ze ia3.err \
+            -zjobid -zmailaddrs lucy.mahony@partners.basf.com \
+            -zqueue gpu.h100 -zasync
 
 
 
